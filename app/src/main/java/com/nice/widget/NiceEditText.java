5bc486f530387a6d77be63f154465333597ab4ce
Chenhr <rong5690001@gmail.com>
2016-01-19 00:39:54 +0800

    add editText

---
+++ app/src/main/java/com/nice/widget/NiceEditText.java
@@ -0,0 +1,23 @@
+package com.nice.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+/**
+ * Created by chen on 2016/1/19.
+ */
+public class NiceEditText extends EditText{
+
+    public NiceEditText(Context context) {
+        super(context);
+    }
+
+    public NiceEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public NiceEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
