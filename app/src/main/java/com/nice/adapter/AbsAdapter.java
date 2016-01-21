62e5a94036c08fcc9e75d33adbfd33386d7cc3e7
Chenhr <rong5690001@gmail.com>
2016-01-21 22:36:26 +0800

    add adapter

---
+++ app/src/main/java/com/nice/adapter/AbsAdapter.java
@@ -0,0 +1,74 @@
+package com.nice.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+/**
+ * Created by chen on 2016/1/19.
+ */
+public abstract class AbsAdapter<T> extends RecyclerView.Adapter<AbsViewHolder> {
+
+    protected List<T> datas;
+    protected Context context;
+    protected int[] layoutId;
+    protected AbsViewHolder[] viewHolders;
+
+    public AbsAdapter(@NonNull List<T> datas, Context context, int... layoutId) {
+        this.datas = datas;
+        addDataAtTop(datas);
+        this.context = context;
+        this.layoutId = layoutId;
+        viewHolders = new AbsViewHolder[layoutId.length];
+    }
+
+    @Override
+    public AbsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        for (int i = 0; i < layoutId.length; i++) {
+            viewHolders[i] =  new AbsViewHolder(LayoutInflater.from(context).inflate(layoutId[i],parent,false));
+        }
+        return viewHolders[viewType];
+    }
+
+    @Override
+    public int getItemCount() {
+        if(datas == null){
+            return 0;
+        }
+        return datas.size();
+    }
+
+    public void addData(List<T> datas){
+        this.datas.addAll(datas);
+    }
+
+    public void addDataAtTop(List<T> datas){
+        this.datas.clear();
+        this.datas.addAll(datas);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public T getItemData(int position){
+        return datas.get(position);
+    }
+
+    public Context getContext(){
+        return context;
+    }
+
+    @Override
+    public abstract int getItemViewType(int position);
+
+    @Override
+    abstract public void onBindViewHolder(AbsViewHolder holder, int position);
+
+
+}
---
+++ app/src/main/java/com/nice/adapter/AbsViewHolder.java
@@ -0,0 +1,81 @@
+package com.nice.adapter;
+
+import android.net.Uri;
+import android.support.v7.widget.RecyclerView;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.facebook.drawee.view.SimpleDraweeView;
+
+/**
+ * Created by chen on 2016/1/19.
+ */
+public class AbsViewHolder extends RecyclerView.ViewHolder{
+
+    private SparseArray<View> views;
+    private View itemView;
+    private View.OnClickListener onClickListener;
+
+    public AbsViewHolder(View itemView) {
+        super(itemView);
+        this.itemView = itemView;
+        views = new SparseArray<View>();
+    }
+
+    public <T extends View> T getView(int id){
+        View view;
+        if(null != views.get(id)){
+            view =  views.get(id);
+        }else{
+            view = itemView.findViewById(id);
+            views.put(id, view);
+        }
+        return (T) view;
+    }
+
+    public AbsViewHolder setText(int id,CharSequence charSequence){
+        View view;
+        TextView textView;
+        if(null != (view = getView(id))){
+            textView = (TextView) view;
+            textView.setText(charSequence);
+        }
+        return this;
+    }
+
+    public AbsViewHolder setImgeByUrl(int id,String url){
+        return this.setImgeByUrl(id, url, 0);
+    }
+
+    public AbsViewHolder setImgeByUrl(int id,String url,float ratdio){
+        View view;
+        SimpleDraweeView draweeView;
+        if(null != (view = getView(id))){
+            draweeView = (SimpleDraweeView) view;
+            draweeView.setImageURI(Uri.parse(url));
+            if (ratdio != 0){
+                draweeView.setAspectRatio(ratdio);
+            }
+        }
+        return this;
+    }
+
+    public AbsViewHolder setImgeByRes(int id,int res){
+        View view;
+        ImageView imageView;
+        if(null != (view = getView(id))){
+            imageView = (ImageView) view;
+            imageView.setImageResource(res);
+        }
+        return this;
+    }
+
+    public void setOnClickListener(View.OnClickListener onClickListener) {
+        this.onClickListener = onClickListener;
+        if (null != onClickListener){
+            itemView.setOnClickListener(onClickListener);
+        }
+    }
+}
