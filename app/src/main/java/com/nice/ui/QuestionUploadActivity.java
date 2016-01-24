fa91080d6e58d6e95140ea3cdde0efca37c01dc8
jiaojiabin <rong5690001@gmail.com>
2016-01-24 19:10:21 +0800

    签名  item_signname.xml   activity_sign_name.xml
    拍照 item_takephoto.xml
    
    选择说明题 item_selectinstruction.xml
    
    单选题 item_signleselect.xml
    
    多选题 item_multiple_select.xml

---
+++ app/src/main/java/com/nice/ui/QuestionUploadActivity.java
@@ -0,0 +1,18 @@
+package com.nice.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+import com.nice.R;
+
+public class QuestionUploadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question_upload);
+
+    }
+
+}
