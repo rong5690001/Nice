a3525e8ece3118e524ec0cb419c29b440ffd77aa
jiaojiabin <rong5690001@gmail.com>
2016-01-27 01:17:08 +0800

    拍照

--- app/src/main/java/com/nice/ui/QuestionSignActivity.java
+++ app/src/main/java/com/nice/ui/QuestionSignActivity.java
@@ -4,16 +4,80 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Locale;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.text.format.DateFormat;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.nice.R;
 
 public class QuestionSignActivity extends AppCompatActivity implements View.OnClickListener {
 
+    @SuppressLint("SdCardPath")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_sign);
 
+
+    }
+
+    @SuppressLint("SdCardPath")
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            String sdStatus = Environment.getExternalStorageState();
+            if (!sdStatus.equals(Environment.MEDIA_MOUNTED)) { // 检测sd是否可用
+                Log.i("TestFile",
+                        "SD card is not avaiable/writeable right now.");
+                return;
+            }
+            new DateFormat();
+            String name = DateFormat.format("yyyyMMdd_hhmmss",Calendar.getInstance(Locale.CHINA)) + ".jpg";
+            Toast.makeText(this, name, Toast.LENGTH_LONG).show();
+            Bundle bundle = data.getExtras();
+            Bitmap bitmap = (Bitmap) bundle.get("data");// 获取相机返回的数据，并转换为Bitmap图片格式
+
+            FileOutputStream b = null;
+            File file = new File("/sdcard/Image/");
+            file.mkdirs();// 创建文件夹
+            String fileName = "/sdcard/Image/"+name;
+
+            try {
+                b = new FileOutputStream(fileName);
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, b);// 把数据写入文件
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    b.flush();
+                    b.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+        }
     }
 
     @Override
@@ -22,7 +86,10 @@ public class QuestionSignActivity extends AppCompatActivity implements View.OnCl
             case R.id.sign_commit_btn:
                 startActivity(new Intent(QuestionSignActivity.this, QuestionContextActivity.class));
                 break;
-
+            case R.id.quest_sign_takephoto:
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 1);
+                break;
         }
     }
 }
