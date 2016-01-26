2f891b5c8be2d9da2cfd2f1c77ea9d7dc0a3febc
jiaojiabin <rong5690001@gmail.com>
2016-01-27 00:44:51 +0800

    问卷选项实体类

---
+++ app/src/main/java/com/nice/model/NiceSheetQuestionOption.java
@@ -0,0 +1,22 @@
+package com.nice.model;
+
+import java.io.Serializable;
+
+/**
+ * 问题主题，选项
+ * Created by jiao on 2016/1/27.
+ */
+public class NiceSheetQuestionOption implements Serializable {
+    public String qoId;
+    public String qoIsDefault;
+    public String qoSequence;
+    public String qoStatus;
+    public String qoText;
+    public String qoValue;
+    public String rbTime;
+    public String ruTime;
+    public String sqId;
+
+
+
+}
